6548b06caae64ecfa0de0bea6a145291523b8a3e
Bill Huber <wbhuber@us.ibm.com>
2018-03-06 16:06:26 -0600

    Update version to 2.0.3.dev1

--- ibm_s3transfer/__init__.py
+++ ibm_s3transfer/__init__.py
@@ -143,7 +143,7 @@ from ibm_s3transfer.exceptions import RetriesExceededError, S3UploadFailedError
 
 
 __author__ = 'IBM'
-__version__ = '2.0.2'
+__version__ = '2.0.3.dev1'
 
 
 class NullHandler(logging.Handler):
