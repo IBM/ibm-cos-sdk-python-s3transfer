6dea02764cea128f404dca583c5a122d385ea0e8
seamus.mcgrath <seamus.mcgrath@anaeko.com>
2018-04-30 11:23:48 +0100

    Update version to 2.1.2.dev1

--- ibm_s3transfer/__init__.py
+++ ibm_s3transfer/__init__.py
@@ -143,7 +143,7 @@ from ibm_s3transfer.exceptions import RetriesExceededError, S3UploadFailedError
 
 
 __author__ = 'IBM'
-__version__ = '2.1.1'
+__version__ = '2.1.2.dev1'
 
 
 class NullHandler(logging.Handler):
