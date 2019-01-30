817b719669e0d672e0a77ee8c173591250501fc4
Connell <connell@anaeko.com>
2019-01-30 18:46:27 +0000

    Update version to 2.4.4.dev1

--- ibm_s3transfer/__init__.py
+++ ibm_s3transfer/__init__.py
@@ -143,8 +143,7 @@ from ibm_s3transfer.exceptions import RetriesExceededError, S3UploadFailedError
 
 
 __author__ = 'IBM'
-__version__ = '2.4.3'
-
+__version__ = '2.4.4.dev1'
 
 class NullHandler(logging.Handler):
     def emit(self, record):
