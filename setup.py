a244c0aed3f7641f1bac8301981530da5044131d
Robert T Veitch <rveitch@us.ibm.com>
2020-04-06 13:57:44 -0500

    Drop support for Python 2.7 and 3.4

--- setup.py
+++ setup.py
@@ -1,28 +1,16 @@
 #!/usr/bin/env python
 import os
 import re
-import sys
+
 from setuptools import setup, find_packages
-
-# IbmCos sdk python version check
-_valid  =  sys.version_info[:2] == (2, 7) or sys.version_info >= (3,4)
-if not _valid:
-    sys.exit("Sorry, IBM COS SDK only supports versions 2.7, 3.4, 3.5, 3.6, 3.7 of python.")
-
 
 ROOT = os.path.dirname(__file__)
 VERSION_RE = re.compile(r'''__version__ = ['"]([a-z0-9.]+)['"]''')
-
 
 requires = [
     'ibm-cos-sdk-core==2.6.0',
 ]
 
-if sys.version_info[0] == 2:
-    # concurrent.futures is only in python3, so for
-    # python2 we need to install the backport.
-    requires.append('futures>=2.2.0,<4.0.0')
-    requires.append('backports.functools-lru-cache>=1.5')
 
 def get_version():
     init = open(os.path.join(ROOT, 'ibm_s3transfer', '__init__.py')).read()
@@ -40,6 +28,7 @@ setup(
     url='https://github.com/ibm/ibm-cos-sdk-python-s3transfer',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
+    python_requires='~=3.5',
     install_requires=requires,
     license="Apache License 2.0",
     classifiers=(
@@ -48,11 +37,10 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ),
 )
