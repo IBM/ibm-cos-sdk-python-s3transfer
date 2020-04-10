10a096e6069f40e2700cbe8240b037078da04512
ERIC V TASHAKKOR <Eric.Tashakkor@ibm.com>
2020-04-10 09:44:20 -0500

    Release 2.6.2

--- ibm_s3transfer/__init__.py
+++ ibm_s3transfer/__init__.py
@@ -142,7 +142,7 @@ from ibm_s3transfer.exceptions import RetriesExceededError, S3UploadFailedError
 
 
 __author__ = 'IBM'
-__version__ = '2.6.1'
+__version__ = '2.6.2'
 
 
 class NullHandler(logging.Handler):
