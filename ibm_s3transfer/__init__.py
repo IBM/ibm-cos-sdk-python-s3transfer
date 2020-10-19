1ed70e2aa4e2c37b93a312224ab4df60a651706c
Justin M Jarczyk <jjarczyk@us.ibm.com>
2020-10-19 13:12:48 -0500

    Release 2.8.0

--- ibm_s3transfer/__init__.py
+++ ibm_s3transfer/__init__.py
@@ -142,7 +142,7 @@ from ibm_s3transfer.exceptions import RetriesExceededError, S3UploadFailedError
 
 
 __author__ = 'IBM'
-__version__ = '2.7.0'
+__version__ = '2.8.0'
 
 
 class NullHandler(logging.Handler):
